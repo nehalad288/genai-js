d503af921fbf2ce24aef03d212e13b7dc022abe8
nehalad288 <neha.s.lad@gmail.com>
2025-09-11 17:07:57 -0700

    chaining and parameter controlling

--- src/llmChain/llmChain.ts
+++ src/llmChain/llmChain.ts
@@ -2,8 +2,12 @@ import { PromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import dotenv from "dotenv";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { LLMChain } from "@langchain/chains";
+import { RunnableSequence } from "@langchain/core/runnables";
 
 dotenv.config();
+
+await personalizedPitch("Generative AI", "JavaScript Developer", 50);
 
 async function personalizedPitch(
   course: string,
@@ -22,7 +26,42 @@ async function personalizedPitch(
     wordLimit,
   });
   console.log("Formatted Prompt:", formattedPrompt);
-}
 
-const llm = new ChatOpenAI();
-const outputParser = new StringOutputParser();
+  const llm = new ChatOpenAI({
+    // temperature: 0,
+    // topP: 1,
+    maxTokens: 150,
+    modelName: "gpt-3.5-turbo",
+  });
+  const outputParser = new StringOutputParser();
+
+//   Option 1: Langchain Legacy Chain
+//   const legacyChain = new LLMChain({
+//     prompt: promptTemplate,
+//     llm,
+//     outputParser,
+//   });
+
+//   const answer = await legacyChain.invoke({
+//     course,
+//     role,
+//     wordLimit,
+//   });
+
+  // Option 2: LCEL
+  // const lcelChain = promptTemplate.pipe(llm).pipe(outputParser);
+ const lcelChain = RunnableSequence.from([
+    promptTemplate,
+    llm,
+    outputParser,
+  ]);
+
+  const answer = await lcelChain.invoke({
+    course,
+    role,
+    wordLimit,
+  });
+
+  console.log("Chain Response:", answer);
+  
+}
